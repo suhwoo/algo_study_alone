f643de567cfd30f508a894ccfa11d46b961acf38
suhwoo <rbfrnltls@gamil.com>
2021-07-07 01:53:14 +0900

    감찾는중.. make Node, input nodeNum, lineNum, startPoint

--- 1260_dfsAndBfs.cpp
+++ 1260_dfsAndBfs.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+vector<Node> allNode;
+struct Node {
+	unsigned int nodeName;
+	vector<Node> linkedNode;
+};
+int main() {
+	unsigned int nodeNum;
+	unsigned int lineNum;
+	unsigned int startPoint;
+	cin >> nodeNum >> lineNum >> startPoint;
+
+	return 0;
+}
