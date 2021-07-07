ec4927f75f68db2a8ac3f96e1d0d30b3cfe3c57a
suhwoo <rbfrnltls@gamil.com>
2021-07-08 02:20:44 +0900

    vector에서 반복문 돌리기 싫은데 set으로 해결할 순 없을까.. struct를 어떻게 추상선언해야하는지 모르겠네..

--- 1260_dfsAndBfs.cpp
+++ 1260_dfsAndBfs.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
-vector<Node> allNode;
+struct setByNodeName {};
 struct Node {
 	unsigned int nodeName;
-	vector<Node> linkedNode;
+	bool isVisit = false;
+	set<Node*, setByNodeName> linkedNode;
 };
+struct setByNodeName {
+	bool operator() (const Node* left, const Node* right) const {
+		if (left->nodeName < right->nodeName) {
+			return left->nodeName < right->nodeName;
+		}
+	}
+};
+vector<Node*> allNode;
+int dfs(Node* nowNode) {
+	nowNode->isVisit = true;
+
+}
 int main() {
 	unsigned int nodeNum;
 	unsigned int lineNum;
 	unsigned int startPoint;
+	unsigned int firstPoint = 0;
+	unsigned int otherPoint = 0;
 	cin >> nodeNum >> lineNum >> startPoint;
+	//to fill index 0
+	Node* tempNode = new Node;
+	allNode.push_back(tempNode);
+	for (int i = 1; i <= nodeNum; i++) {
+		Node* tempNode = new Node;
+		tempNode->nodeName = i;
+		allNode.push_back(tempNode);
+	}
+	for (int i = 0; i < lineNum; i++) {
+		cin >> firstPoint >> otherPoint;
+		allNode[firstPoint]->linkedNode.insert(allNode[otherPoint]);
+		allNode[otherPoint]->linkedNode.insert(allNode[firstPoint]);
+	}
+	dfs(allNode[startPoint]);
 
 	return 0;
 }
